10b6de5d13ad8735179dc38022049dbd9c4273b7
Syed Minam Ur Rehman <138588241+minamrahman727@users.noreply.github.com>
2025-04-08 21:11:23 +0500

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,12 +1,32 @@
 import Link from 'next/link';
+import { FaFacebook, FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa6';
 
 export default function Footer() {
-    return (
-      <footer className="bg-blue-900 text-white p-4 text-center mt-6">
-        <p>Information provided by <Link href="https://cheeltech.com/" className="underline">CheelTech</Link></p>
-        <p>Developed by <Link href="https://smrehman.vercel.app/" className="underline">Syed Minam Ur Rehman</Link></p>
-        <p>&copy; 2025 ZEX software solutions. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="bg-gradient-to-r from-cyan-600 via-blue-800 to-indigo-900 text-white p-6 text-center mt-6">
+      <div className="mb-4 flex justify-center space-x-4">
+        <Link href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+          <FaFacebook className="text-3xl  transition duration-300" />
+        </Link>
+        <Link href="https://instagram.com/syedminamurrehman" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+          <FaInstagram className="text-3xl  transition duration-300" />
+        </Link>
+        <Link href="https://linkedin.com/in/syedminamurrehman" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+          <FaLinkedin className="text-3xl  transition duration-300" />
+        </Link>
+        <Link href="https://github.com/minamrahman727/rahguzar" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
+          <FaGithub className="text-3xl   transition duration-300" />
+        </Link>
+         <Link href="mailto:minamrahman727@gmail.com?subject=Inquiry%20about%20Rahguzar" target="_blank" rel="noopener noreferrer" aria-label="Email">
+          <FaEnvelope className="text-3xl  transition duration-300" />
+        </Link>
+      </div>
+      {/* <p className='font-serif text-base'>Developed by Syed Minam Ur Rehman</p> */}
+      <p className=' text-xs'>Information provided by <Link href="https://cheeltech.com" target="_blank" rel="noopener noreferrer" className="hover:text-cyan-200 transition duration-300">Cheeltech</Link></p>
+      <p className='text-sm '>&copy; {currentYear} Syed Minam Ur Rehman. All rights reserved.</p>
+    </footer>
+  );
+}
