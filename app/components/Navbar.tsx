585771f504679f32559d8fc94cd43f283fc1e75b
Syed Minam Ur Rehman <138588241+minamrahman727@users.noreply.github.com>
2025-04-08 21:19:42 +0500

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <nav className="bg-blue-600 text-white p-4 fixed w-full top-0 shadow-lg z-50">
+    <nav className="bg-transparent text-white p-4 fixed w-full top-0  z-50">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center space-x-3">
+        <div className="flex items-center space-x-3   ">
           <Image src="/images/route.png" alt="RahGuzar Logo" width={50} height={50} />
-          <h1 className="text-2xl font-bold">RahGuzar</h1>
+          <h1 className="text-2xl font-bold drop-shadow-lg">RahGuzar</h1>
         </div>
       </div>
     </nav>
