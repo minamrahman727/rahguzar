29c6149cfd51cd33e05c066c7bc1902059008a0b
Syed Minam Ur Rehman <138588241+minamrahman727@users.noreply.github.com>
2025-05-04 01:55:40 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import NotificationBanner from "./components/banner";
 import Routes from "./components/Routes";
 import Footer from "./components/Footer";
 
@@ -8,7 +9,7 @@ export default function Home() {
     <div>
       <Navbar />
       <Hero />
-      
+      <NotificationBanner />
       <Routes />
       <Footer/>
     </div>
